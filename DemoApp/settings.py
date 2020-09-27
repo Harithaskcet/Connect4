4ab34b440af364444ac2d941a83c3da8a751676c
Haritha <haritha.msk@mrcooper.com>
2020-09-27 13:47:22 +0530

    'set_debug_true'

--- DemoApp/settings.py
+++ DemoApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vxqz5levpt(5p_8yh5m574o(ms=dxn&d113^iu*-jyh4dbar3%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
