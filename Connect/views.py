c1ffa8e424a98e45242ef2e9b471678e4c8d3324
Haritha <haritha.msk@mrcooper.com>
2020-09-27 13:52:56 +0530

    'added_method_request'

--- Connect/views.py
+++ Connect/views.py
@@ -52,98 +52,99 @@ def createUser(request, player1, player2):
         'PlayerB': player2 })
 
 def update(request, row, col):
-    global player, playerB
-    print(player)
-    userA = get_object_or_404(Connect, userName=playerA)
-    userB = get_object_or_404(Connect, userName=playerB)
-    mat = userA.grid 
-    if player == 1:
-        for x in range(5,-1,-1):
-            print(mat[x][col])
-            if mat[x][col] == 0:
-                mat[x][col] = 1
-                move = userA.moves
-                move.append(col+1)
-                userA.moves = move
-                userA.grid = mat
-                userB.grid = mat
-                player = 2
-                userA.save()
-                userB.save()
-                print(userA.grid)
-                if patternSearch(userA.grid, [1,1,1,1]):
-                    return render(request, 'GamePlay.html', {
-                        'checks': userA.grid,
-                        'row': [0,1,2,3,4,5],
-                        'col':[0,1,2,3,4,5,6],
-                        'turn': 'Yellow Won!',
-                        'Amoves':move,
-                        'Bmoves':userB.moves,
-                        'PlayerA':userA.userName,
-                        'PlayerB':playerB })
-                else:
-                    return render(request, 'GamePlay.html', {
-                        'checks': userA.grid,
-                        'row': [0,1,2,3,4,5],
-                        'col':[0,1,2,3,4,5,6],
-                        'turn': 'Red Turn',
-                        'Amoves':move,
-                        'Bmoves':userB.moves,
-                        'PlayerA':userA.userName,
-                        'PlayerB': playerB  })
-        return render(request, 'GamePlay.html', {
-            'checks': userA.grid,
-            'row': [0,1,2,3,4,5],
-            'col':[0,1,2,3,4,5,6],
-            'turn': 'Yellow Turn',
-            'error': 'Invalid Move',
-            'Amoves':userA.moves,
-            'Bmoves':userB.moves,
-            'PlayerA':userA.userName,
-            'PlayerB':playerB })
-    else:
-        for x in range(5,-1,-1):
-            if mat[x][col] == 0:
-                mat[x][col] = 2
-                move = userB.moves
-                move.append(col+1)
-                userB.moves = move
-                userA.grid = mat
-                userB.grid = mat
-                player = 1
-                userA.save()
-                userB.save()
-                print(userB.moves)
-                if patternSearch(userA.grid, [2,2,2,2]):
-                    return render(request, 'GamePlay.html', {
-                        'checks': userA.grid,
-                        'row': [0,1,2,3,4,5],
-                        'col':[0,1,2,3,4,5,6],
-                        'turn': 'Red Won!',
-                        'Amoves':userA.moves,
-                        'Bmoves':move,
-                        'PlayerA':userA.userName,
-                        'PlayerB':playerB })
-                else:
-                    return render(request, 'GamePlay.html', {
-                        'checks': userA.grid,
-                        'row': [0,1,2,3,4,5],
-                        'col':[0,1,2,3,4,5,6],
-                        'turn': 'Yellow Turn',
-                        'Amoves':userA.moves,
-                        'Bmoves':move,
-                        'PlayerA':userA.userName,
-                        'PlayerB':playerB  })
-        return render(request, 'GamePlay.html', {
-            'checks': userA.grid,
-            'row': [0,1,2,3,4,5],
-            'col':[0,1,2,3,4,5,6],
-            'turn': 'Red Turn',
-            'error': 'Invalid Move',
-            'Amoves':userA.moves,
-            'Bmoves':userB.moves,
-            'PlayerA':userA.userName,
-            'PlayerB':playerB  })
+    if request.method == 'GET' or request.method == 'POST':
+        global player, playerB
+        print(player)
+        userA = get_object_or_404(Connect, userName=playerA)
+        userB = get_object_or_404(Connect, userName=playerB)
+        mat = userA.grid 
+        if player == 1:
+            for x in range(5,-1,-1):
+                print(mat[x][col])
+                if mat[x][col] == 0:
+                    mat[x][col] = 1
+                    move = userA.moves
+                    move.append(col+1)
+                    userA.moves = move
+                    userA.grid = mat
+                    userB.grid = mat
+                    player = 2
+                    userA.save()
+                    userB.save()
+                    print(userA.grid)
+                    if patternSearch(userA.grid, [1,1,1,1]):
+                        return render(request, 'GamePlay.html', {
+                            'checks': userA.grid,
+                            'row': [0,1,2,3,4,5],
+                            'col':[0,1,2,3,4,5,6],
+                            'turn': 'Yellow Won!',
+                            'Amoves':move,
+                            'Bmoves':userB.moves,
+                            'PlayerA':userA.userName,
+                            'PlayerB':playerB })
+                    else:
+                        return render(request, 'GamePlay.html', {
+                            'checks': userA.grid,
+                            'row': [0,1,2,3,4,5],
+                            'col':[0,1,2,3,4,5,6],
+                            'turn': 'Red Turn',
+                            'Amoves':move,
+                            'Bmoves':userB.moves,
+                            'PlayerA':userA.userName,
+                            'PlayerB': playerB  })
+            return render(request, 'GamePlay.html', {
+                'checks': userA.grid,
+                'row': [0,1,2,3,4,5],
+                'col':[0,1,2,3,4,5,6],
+                'turn': 'Yellow Turn',
+                'error': 'Invalid Move',
+                'Amoves':userA.moves,
+                'Bmoves':userB.moves,
+                'PlayerA':userA.userName,
+                'PlayerB':playerB })
+        else:
+            for x in range(5,-1,-1):
+                if mat[x][col] == 0:
+                    mat[x][col] = 2
+                    move = userB.moves
+                    move.append(col+1)
+                    userB.moves = move
+                    userA.grid = mat
+                    userB.grid = mat
+                    player = 1
+                    userA.save()
+                    userB.save()
+                    print(userB.moves)
+                    if patternSearch(userA.grid, [2,2,2,2]):
+                        return render(request, 'GamePlay.html', {
+                            'checks': userA.grid,
+                            'row': [0,1,2,3,4,5],
+                            'col':[0,1,2,3,4,5,6],
+                            'turn': 'Red Won!',
+                            'Amoves':userA.moves,
+                            'Bmoves':move,
+                            'PlayerA':userA.userName,
+                            'PlayerB':playerB })
+                    else:
+                        return render(request, 'GamePlay.html', {
+                            'checks': userA.grid,
+                            'row': [0,1,2,3,4,5],
+                            'col':[0,1,2,3,4,5,6],
+                            'turn': 'Yellow Turn',
+                            'Amoves':userA.moves,
+                            'Bmoves':move,
+                            'PlayerA':userA.userName,
+                            'PlayerB':playerB  })
+            return render(request, 'GamePlay.html', {
+                'checks': userA.grid,
+                'row': [0,1,2,3,4,5],
+                'col':[0,1,2,3,4,5,6],
+                'turn': 'Red Turn',
+                'error': 'Invalid Move',
+                'Amoves':userA.moves,
+                'Bmoves':userB.moves,
+                'PlayerA':userA.userName,
+                'PlayerB':playerB  })
 
 def search2D(grid, row, col, word): 
     dirs = [[-1, 0], [1, 0], [1, 1],[1, -1], [-1, -1], [-1, 1],[0, 1], [0, -1]] 
